296beb5e21783747bf7a1341055ddda158b62b66
Okan Dogan <okandogan1999@gmail.com>
2021-12-10 01:59:55 +0300

    Created models
    Created models, some configurations.

---
+++ Logiwa_CaseStudy/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logiwa_CaseStudy.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        [NotNull]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        [Required]
+        public virtual Category category { get; set; }
+
+        public int CategoryID { get; set; }
+    }
+}
